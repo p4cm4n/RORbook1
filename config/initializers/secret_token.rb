434ab4ae52df5c24f9d3dee6c3af74eed4ac70f7
Ti53 <soporte@ti53.es>
2012-11-20 10:46:28 +0100

    sfdgsdfg

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RORbook1::Application.config.secret_token = '79255f7d574797fc0fbc02d2ecb496693f8c68064c0783dc250d93a9a4891d6a94e4e37fbbc2d55e2b25520a4cce6216a5839f84cefd1175397bc1c899a345ef'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+RORbook1::Application.config.session_store :cookie_store, key: '_RORbook1_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# RORbook1::Application.config.session_store :active_record_store
